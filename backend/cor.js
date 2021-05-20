cf83b39338b5657bd27000814e14bc6d0f8832f2
Xavier2414 <xaviertomaspons03@gmail.com>
2021-05-20 22:35:35 +0200

    intento de funcionar guardar datos

---
+++ backend/cor.js
@@ -0,0 +1,10 @@
+class cor {
+    constructor(nombre, email, alias, sobreti) {
+      this.nombre = nombre;
+      this.email = email;
+      this.alias = alias;
+      this.sobreti = sobreti;
+    }
+  }
+  
+  export default cor;
